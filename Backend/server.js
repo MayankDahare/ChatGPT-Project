a56dd6cdca9e10e0ed287e91778cafcd26b5eff3
Mayank Dahare <mayankdahare56@gmail.com>
2025-11-16 14:35:49 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/db/db');
+const initializeSocketServer = require('./src/sockets/socket.server');
+const httpServer = require('http').createServer(app) ;
+
+
+// Connect to the database
+connectDB();
+// Initialize Socket Server
+initializeSocketServer(httpServer);
+
+
+
+
+httpServer.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
---
+++ Backend/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// routes
+const authRoutes = require('./routes/auth.routes');
+const chatRoutes = require('./routes/chat.routes');
+
+
+
+const app = express();
+
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
+
+// uaing middlewares 
+app.use(express.json());
+app.use(cookieParser());
+
+// using routes 
+app.use('/api/auth', authRoutes);
+app.use('/api/chat', chatRoutes);
+
+module.exports = app;
