f89e91356f5208507e2818786d4df6351266c734
Mayank Dahare <mayankdahare56@gmail.com>
2025-11-16 20:45:52 +0530

    part-5

--- Backend/server.js
+++ Backend/server.js
@@ -2,17 +2,18 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 const initializeSocketServer = require('./src/sockets/socket.server');
-const httpServer = require('http').createServer(app) ;
-
+const httpServer = require('http').createServer(app);
 
 // Connect to the database
 connectDB();
 // Initialize Socket Server
 initializeSocketServer(httpServer);
 
+// Render ke liye correct port handling
+const PORT = process.env.PORT || 4000;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
-
-httpServer.listen(4000, () => {
-  console.log('Server is running on port 4000');
-}); 
