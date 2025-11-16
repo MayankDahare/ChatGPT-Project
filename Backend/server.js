daf5d79c274a882715d186e5aaf6dc0c20baf724
Mayank Dahare <mayankdahare56@gmail.com>
2025-11-16 19:18:44 +0530

    part-2

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
+const path = require("path");
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 const initializeSocketServer = require('./src/sockets/socket.server');
-const httpServer = require('http').createServer(app) ;
-
+const httpServer = require('http').createServer(app);
 
 // Connect to the database
 connectDB();
+
 // Initialize Socket Server
 initializeSocketServer(httpServer);
 
 
+// ------------ ADD THIS PART (Frontend Serve Code) -------------
+const publicPath = path.join(__dirname, "Backend", "public");
+
+// Serve static React files
+app.use(express.static(publicPath));
+
+// Always return index.html for any unknown route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+// ---------------------------------------------------------------
 
 
 httpServer.listen(4000, () => {
